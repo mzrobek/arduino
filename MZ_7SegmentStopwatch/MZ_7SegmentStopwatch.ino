1fe44772aac43d7063d8c440f8b72391e9075092
Maciej Zrobek <maciej.zrobek@pl.ibm.com@P40227-MBP.local>
2020-11-11 17:59:36 +0100

    Reset switch working

--- MZ_7SegmentStopwatch/MZ_7SegmentStopwatch.ino
+++ MZ_7SegmentStopwatch/MZ_7SegmentStopwatch.ino
@@ -5,19 +5,22 @@
 
   The stopwatch can measurre time up to 10 minutes and displays the running time in m.ss.d format (d is for deciseconds)
   Initially the stopwatch displays 0.00.0
-  Pressing the switch starts/pauses the stopwatch
-  After 10 minutes are reached the stopwatch displays ----
+  Pressing the 1st switch starts/pauses the stopwatch
+  After 10 minutes reached the stopwatch displays ---- (overflow)
+  While the stopwatch is paused or oveflown pressing the 2nd switch resets the stopwatch to zero
 
 */
 
-#define MINUTE 60
+#define MINUTE            60
+#define OVERFLOW_MINUTES  10
 // Timer machine states
 #define ST_READY      0   // Stopwatch not yet started
 #define ST_RUNNING    1   // Stopwatch started and running
 #define ST_PAUSED     2   // Stopwatch paused
 #define ST_OVERFLOW   3   // Stopwatch time has exceeded 10 minutes
 
-#define SWITCH_PIN    A0  // Pin to which the tact switch is connected
+#define SWITCH1_PIN    A0  // Start/Stop switch
+#define SWITCH2_PIN    A1  // Reset switch
 
 #define STR_ZEROES    "0.00.0"
 #define STR_OVERFLOW  "----"
@@ -40,8 +43,9 @@ void setup() {
   bool leadingZeros = true; // Use 'true' if you'd like to keep the leading zeros
   bool disableDecPoint = false; // Use 'true' if your decimal point doesn't exist or isn't connected
 
-  pinMode(SWITCH_PIN, INPUT);
-  
+  pinMode(SWITCH1_PIN, INPUT);
+  pinMode(SWITCH2_PIN, INPUT);
+
   // LED initialisation
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
                updateWithDelays, leadingZeros, disableDecPoint);
@@ -49,29 +53,32 @@ void setup() {
 }
 
 void loop() {
-  
-  static unsigned long timer = millis();  // Current time from starting Arduino (not the stopwatch)
+
+  static unsigned long timer = 0UL;  // Time in ms elapsed from starting the stopwatch
   static int deciSeconds = 0;
   static byte pgmState = ST_READY;
   static byte prevPgmState = ST_READY;
   int seconds = 0;
   int minutes = 0;
   int tenthsOfSec = 0;
-  char displayString[7]; 
-  byte switchState;
-  static byte prevSwitchState = LOW;
+  char displayString[7];
+  byte switchState1, switchState2;
+  static byte prevSwitchState1 = LOW;
+  static byte prevSwitchState2 = LOW;
 
-  switchState = digitalRead(SWITCH_PIN);
+  switchState1 = digitalRead(SWITCH1_PIN);
+  switchState2 = digitalRead(SWITCH2_PIN);
 
   switch (pgmState)
   {
     case ST_READY:
       // We start the operation with displaying 0.00.0
       strcpy(displayString, STR_ZEROES);
-      if (switchState == HIGH && prevSwitchState == LOW) // Double check to deal with long switch press
+      if (switchState1 == HIGH && prevSwitchState1 == LOW) // Double check to deal with long switch press
       {
         if (pgmState == prevPgmState)
         {
+          timer = millis();
           pgmState = ST_RUNNING; // Start the stopwatch
         }
         prevPgmState = pgmState;
@@ -81,11 +88,11 @@ void loop() {
     case ST_RUNNING:
       if (millis() - timer >= 100)
       {
-        // Noemal stopwatch operation
+        // Normal stopwatch operation
         timer += 100;
         deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
 
-        if (deciSeconds == 10 * MINUTE * 10)
+        if (deciSeconds == OVERFLOW_MINUTES * MINUTE * 10)
         {
           // Display dashes when 10 minutes are reached
           pgmState = ST_OVERFLOW;
@@ -100,8 +107,8 @@ void loop() {
           sprintf(displayString, "%d.%02d.%d", minutes, seconds, tenthsOfSec);
         }
       }
-      
-      if (switchState == HIGH && prevSwitchState == LOW)
+
+      if (switchState1 == HIGH && prevSwitchState1 == LOW)
       {
         if (pgmState == prevPgmState)
         {
@@ -113,18 +120,39 @@ void loop() {
 
     case ST_PAUSED:
       timer = millis(); // If we don't update the timer the stopwatch will be still running in the background
-      if (switchState == HIGH && prevSwitchState == LOW)
+      if (switchState1 == HIGH && prevSwitchState1 == LOW)
       {
         if (pgmState == prevPgmState)
         {
           pgmState = ST_RUNNING; // Unpause the stopwatch
         }
         prevPgmState = pgmState;
+        break;
+      }
+
+      if (switchState2 == HIGH && prevSwitchState2 == LOW)
+      {
+        if (pgmState == prevPgmState)
+        {
+          // Reset the stopwatch
+          deciSeconds = 0;
+          pgmState = ST_READY;
+        }
+        prevPgmState = pgmState;
+        break;
       }
 
       break;
-      
+
     case ST_OVERFLOW:
+      if (switchState2 == HIGH && prevSwitchState2 == LOW)
+      {
+        // Reset the stopwatch
+        deciSeconds = 0;
+        pgmState = ST_READY;
+        prevPgmState = pgmState;
+        break;
+      }
       strcpy(displayString, STR_OVERFLOW);
       break;
   }
@@ -132,7 +160,8 @@ void loop() {
   sevseg.setChars(displayString);
   sevseg.refreshDisplay(); // Must run repeatedly
 
-  prevSwitchState = switchState;
+  prevSwitchState1 = switchState1;
+  prevSwitchState2 = switchState2;
   delay (5); // To avoid flickering
 }
 
